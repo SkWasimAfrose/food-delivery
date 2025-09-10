fc0ddc3111890b3c969f4db07dc3d195fafc25e6
WasimAfrose <wasimafrose2020@gmail.com>
2025-09-10 16:55:37 +0530

    add pwa support

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+// This is a basic service worker for PWA support
+self.addEventListener("install", function (event) {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", function (event) {
+  // You can add custom caching logic here if needed
+});
